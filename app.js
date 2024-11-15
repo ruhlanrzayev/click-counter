49b032729555bbf1f98e29834a385014c7631e36
Ruhlan Rzayev <contactruhlanrzayev@gmail.com>
2024-11-15 14:08:19 +0400

    updates new version

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+document.body.innerHTML = `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Click Counter | Rzayev Ruhlan</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="center-container">
+        <label id="countLabel">0</label> <br>
+        <div id="container">
+            <button class="button" id="decreaseBtn">Decrease</button>
+            <button class="button" id="resetBtn">Reset</button>
+            <button class="button" id="increaseBtn">Increase</button>
+        </div>
+    </div>
+    <script src="app.js"></script>
+</body>
+
+</html>
+`;
+
 let count = 0;
 
 document.getElementById("decreaseBtn").onclick = function () {
